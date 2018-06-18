500beb5356210891b5e5a7de8290f110dc4490e9
John Allison <john.allison@noblerc.com>
2018-06-18 15:05:29 -0400

    Completed Chapter 6 excersizes.

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,14 +1,56 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rango.models import Category
+from rango.models import Category, Page
+
 
 # Create your views here.
 def index(request):
+    # Query the database for a list of ALL categoris currently stored.
+    # Order the categories by no. likes in descening order
+    # Retrieve the top 5 only - or all if less than 5
+    # Place the list in our context_dict dictinary
+    # that will be passed to the template engine.
     
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    pages_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages':pages_list}
+
+    # Render the response and send it back!
     return render(request, 'index.html', context_dict)
 
 def about(request):
     context_dict = {'boldmessage': "This is part of the tutorial!"}
     return render(request, 'about.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    # Create a context dictionary which we can pass
+    # to the template rendering engine
+    context_dict = {}
+
+    try:
+        # Can we find a category name slug with the given name?
+        # If we can't, the .get() method raises a DoesNotExist exceptiono.
+        # So the .get() methond returns one model instance or raises and exception.
+        category = Category.objects.get(slug=category_name_slug)
+
+        # Retrieve all of the associated pages.
+        # Note that filter() will return a list of page objects or an empty list
+        pages = Page.objects.filter(category=category)
+
+        # Adds our results list to the template context under name pages.
+        context_dict['pages'] = pages
+
+        # We allso add the category object from
+        # the database to the context dictionary.
+        # We'll use this in the template to verify that the category exists.
+        context_dict['category'] = category
+
+    except Cateogry.DoesNotExist:
+        # We get here if we didn't find the specified category
+        # Don't do anything-
+        # the template will display the "no category" message for us.
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    # Go render the response and return it to the client.
+    return render(request, 'category.html', context_dict)
